2981dc07f606bf4abd0468db0840b398a410e359
Alexandre Israël <bonjour@alexandre-israel.com>
2018-06-12 15:39:29 +0200

    Code inutile

--- src/XtendedContent/Serve/XtcRequest/ElasticaRequest.php
+++ src/XtendedContent/Serve/XtcRequest/ElasticaRequest.php
@@ -11,27 +11,5 @@ namespace Drupal\xtc\XtendedContent\Serve\XtcRequest;
 
 class ElasticaRequest extends AbstractXtcRequest
 {
-  /**
-   * @param $method
-   * @param string $param
-   *
-   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|mixed|string
-   */
-  public function get($method, $param = '')
-  {
-    // @TODO: Do it the ES way.
-    if ($this->isAllowed($method)){
-      try {
-        $this->getClient()->setUri($method, $param);
-        $content = $this->getClient()->get();
-        $this->setData($content);
-        return $this;
-      } catch (RequestException $e) {
-        return ('Request error: ' . $e->getMessage());
-      }
-    }
-    else{
-      return (t('Request error: The "'.$method.'" method is not allowed.'));
-    }
-  }
+
 }
